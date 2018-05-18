6673a4bfc7722324db4a9437893ec9cf23b76e58
pranavenzo <panappin@purdue.edu>
2018-05-18 07:44:01 -0700

    fin experiment p1

--- homestuffs/neural_net_learning_visual.py
+++ homestuffs/neural_net_learning_visual.py
@@ -3,31 +3,53 @@ import numpy as np
 
 from sklearn.neural_network import MLPRegressor
 
-
 def get_reward_for_state(X):
     return X[0][0] ** 2 - X[0][1] ** 2
 
-
-def get_random_state_representation_with_target():
+def get_random_state_representation():
     f1, f2 = (random.random() * 20, random.random() * 20)
     return f1, f2
 
-
 model = MLPRegressor()
-X = get_random_state_representation_with_target()
+X = [15, 0]
 X = np.array(X).reshape(1, -1)
-print('Initial Oracle X: ', X)
 model.partial_fit(X, np.array([0, 0]).reshape(1, -1))
-new_X = get_random_state_representation_with_target()
+print('Initial Oracle X: ', X)
+new_X = [0, 15]
 new_X = np.array(new_X).reshape(1, -1)
-print(new_X,  model.predict(new_X))
-smaller = min(abs(get_reward_for_state(X)), abs(get_reward_for_state(new_X)))
-bigger = max(abs(get_reward_for_state(X)), abs(get_reward_for_state(new_X)))
+print('New point new_X', new_X)
+print('Old pred vs new pred', model.predict(X)[0], model.predict(new_X)[0])
+smaller = min(get_reward_for_state(X), get_reward_for_state(new_X))
+bigger = max(get_reward_for_state(X), get_reward_for_state(new_X))
+
 scale = smaller / bigger
+if abs(scale) > 1:
+    scale = 1.0 / scale
 learning_rate = 0.01
-direction_vector = X - new_X
-target_for_new_x = scale * learning_rate * direction_vector + X
-print(target_for_new_x)
+direction_vector = model.predict(new_X)[0] - model.predict(X)[0]
+target_for_new_x = scale * learning_rate * direction_vector + model.predict(X)[0]
+print('---X vs new_X---')
+print(scale)
+print(direction_vector)
+print('----------------')
 model.partial_fit(new_X, np.array(target_for_new_x).reshape(1, -1))
-print(model.predict(new_X))
-print(model.predict(X))
+print('Old pred vs new pred', model.predict(X)[0], model.predict(new_X)[0])
+
+newnew_X = [15, 15]
+newnew_X = np.array(newnew_X).reshape(1, -1)
+print('New point newnew_X', newnew_X)
+print('Old pred vs new pred vs newnew pred', model.predict(X)[0], model.predict(new_X)[0], model.predict(newnew_X)[0])
+smaller = min(get_reward_for_state(X), get_reward_for_state(newnew_X))
+bigger = max(get_reward_for_state(X), get_reward_for_state(newnew_X))
+scale = smaller / bigger
+if abs(scale) > 1:
+    scale = 1.0 / scale
+learning_rate = 0.01
+direction_vector = model.predict(newnew_X)[0] - model.predict(X)[0]
+target_for_new_x = scale * learning_rate * direction_vector + model.predict(X)[0]
+print('---X vs new_X---')
+print(scale)
+print(direction_vector)
+print('----------------')
+model.partial_fit(newnew_X, np.array(target_for_new_x).reshape(1, -1))
+print('Old pred vs new pred vs newnew pred', model.predict(X)[0], model.predict(new_X)[0], model.predict(newnew_X)[0])
